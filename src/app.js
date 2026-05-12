a8fe31f123990d5e3f31ab8178e326c55e21c122
94070anshika@gmail.com <anshi@email.com>
2026-05-12 15:50:34 +0530

    new commit

--- src/app.js
+++ src/app.js
@@ -5,9 +5,15 @@ app.use("/home",(req, res)=>{
     res.send("home bunny")
 })
 
+app.use("/login", (req,res)=>{
+    res.send("bunny this is login page")
+})
+
+
 app.use("/", (req,res)=>{
     res.send("bunny this is home page new page smjha n teko")
 })
+
 
 app.listen(3000,()=>{
 console.log("server is sucessfully listen on port 3000")
