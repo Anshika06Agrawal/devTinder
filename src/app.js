28b5078eb99f8e0f817b9ad628aa9a39d295cdb9
94070anshika@gmail.com <anshi@email.com>
2026-05-12 19:38:14 +0530

    exploring get, post

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
 const express=require("express");
 const app=express()
 
-app.use("/home",(req, res)=>{
+app.get("/home",(req, res)=>{
+    res.send({firstName:"Anshika" , lastname: "Agrawal"})
+})
+app.post("/update",async(req, res)=>{
+    console.log(req.body)
     res.send("home bunny")
 })
 
+
 app.use("/login", (req,res)=>{
     res.send("bunny this is login page")
-})
+})  
 
 
 app.use("/", (req,res)=>{
