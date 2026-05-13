40b42eb38b6374a14f90ecf193601b6426629ab1
94070anshika@gmail.com <anshi@email.com>
2026-05-13 11:51:32 +0530

    routing&request handling

--- src/app.js
+++ src/app.js
@@ -1,20 +1,34 @@
-const express=require("express");
-const app=express()
+const express = require("express");
+const app = express();
 
-app.get("/user",(req, res)=>{
-    console.log(req.query)
-    res.send({firstName:"Anshika" , lastname: "Agrawal"})
-})
-app.post("/update",async(req, res)=>{
-    console.log(req.body)
-    res.send("home bunny")
-})
+app.get(
+  "/user",
+  (req, res, next) => {
+    console.log(req.query);
+    // res.send({firstName:"Anshika" , lastname: "Agrawal"})
+    next();
+  },
+  (req, res, next) => {
+    console.log(req.query);
+    // res.send({firstName:"Anshika" , lastname: "Agrawal"})
+    next();
+  },
+  (req, res, next) => {
+    console.log(req.query);
+    console.log("4th one called");
+    res.send({ firstName: "Anshika", lastname: "Agrawal" });
+    next();
+  },
+);
+app.post("/update", async (req, res) => {
+  console.log(req.body);
+  res.send("home bunny");
+});
 
+app.use("/login", (req, res) => {
+  res.send("bunny this is login page");
+});
 
-app.use("/login", (req,res)=>{
-    res.send("bunny this is login page")
-})  
-
-app.listen(3000,()=>{
-console.log("server is sucessfully listen on port 3000")
-})
+app.listen(3000, () => {
+  console.log("server is sucessfully listen on port 3000");
+});
