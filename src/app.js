6f7edf26ad646f980f49c8884294d976a1a57a9a
94070anshika@gmail.com <anshi@email.com>
2026-05-15 18:20:02 +0530

    dyanmic data in singup api

--- src/app.js
+++ src/app.js
@@ -2,22 +2,20 @@ const express = require("express");
 const app = express();
 const connectDb = require("./config/dataBase");
 const User = require("./models/user");
-app.use(express.json());  //POST request ka body read karta hai
+app.use(express.json()); //POST request ka body read karta hai
 
 //async use kiya kyunki DB operation time leta hai
 app.post("/signUp", async (req, res) => {
   //creating a new instance of User model
   //User=({}) means User ko ek object pass kar rahe ho. object wrapped inside parentheses
-  const user = new User({
-    //ek naya user document create ho raha hai
-    firstName: "Mahi",
-    lastName: "Dhoni",
-    emailId: "Dhoni@123",
-    Password: "123",
-  });
-  // await use kiya because save() Promise return karta hai aur DB operation complete hone ka wait karna hota hai. ->“await pauses execution until the Promise resolves.”
-  await user.save();
-  res.send("anshika added sucessfully");
+  const user = new User(req.body); //ab pura data middleware se aarha
+  //ek naya user document create ho raha hai
+  try {
+    await user.save();
+    res.send("User added sucessfully");
+  } catch (err) {
+    res.status(400).send("error saving the user:" + err.message);
+  }
 });
 
 connectDb()
